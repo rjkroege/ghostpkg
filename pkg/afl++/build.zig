188bd75efa66df7663753f637633ca76b686612c
Mitchell Hashimoto <m@mitchellh.com>
2026-02-28 20:22:47 -0800

    pkg/afl++: clean up, comments

--- pkg/afl++/build.zig
+++ pkg/afl++/build.zig
@@ -1,24 +1,36 @@
 const std = @import("std");
 
+/// Creates a build step that produces an AFL++-instrumented fuzzing
+/// executable.
+///
+/// Returns a `LazyPath` to the resulting fuzzing executable.
 pub fn addInstrumentedExe(
     b: *std.Build,
     obj: *std.Build.Step.Compile,
 ) std.Build.LazyPath {
-    const pkg = b.dependencyFromBuildZig(@This(), .{});
+    // Force the build system to produce the binary artifact even though we
+    // only consume the LLVM bitcode below. Without this, the dependency
+    // tracking doesn't wire up correctly.
+    _ = obj.getEmittedBin();
 
-    const run_afl_cc = b.addSystemCommand(&.{
+    const pkg = b.dependencyFromBuildZig(
+        @This(),
+        .{},
+    );
+
+    const afl_cc = b.addSystemCommand(&.{
         b.findProgram(&.{"afl-cc"}, &.{}) catch
             @panic("Could not find 'afl-cc', which is required to build"),
         "-O3",
     });
-    _ = obj.getEmittedBin(); // hack around build system bug
-    run_afl_cc.addArg("-o");
-    const fuzz_exe = run_afl_cc.addOutputFileArg(obj.name);
-    run_afl_cc.addFileArg(pkg.path("afl.c"));
-    run_afl_cc.addFileArg(obj.getEmittedLlvmBc());
+    afl_cc.addArg("-o");
+    const fuzz_exe = afl_cc.addOutputFileArg(obj.name);
+    afl_cc.addFileArg(pkg.path("afl.c"));
+    afl_cc.addFileArg(obj.getEmittedLlvmBc());
     return fuzz_exe;
 }
 
+// Required so `zig build` works although it does nothing.
 pub fn build(b: *std.Build) !void {
     _ = b;
 }
